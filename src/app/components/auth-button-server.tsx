47c32b4d51786a3738e36f2a92993bf941e254da
Stefano <hemetmas@gmail.com>
2024-12-19 14:31:37 +0100

    test

---
+++ src/app/components/auth-button-server.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { AuthButton } from './auth-button-client';
+
+// Componente AuthButtonServer que se ejecuta en el servidor
+export async function AuthButtonServer() {
+  // Crea una instancia del cliente de Supabase para el servidor
+  const supabase = createServerComponentClient({ cookies });
+
+  // Obtiene la sesión actual del usuario
+  const { data: { session } } = await supabase.auth.getSession();
+
+  // Renderiza el componente AuthButton pasando la sesión como prop
+  return <AuthButton session={session} />;
+}
