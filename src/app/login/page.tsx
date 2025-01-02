b0682b455efb5e3766dafe213d3e85559c65177b
Stefano <hemetmas@gmail.com>
2025-01-02 23:28:47 +0100

    .

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,12 @@
 import { AuthButtonServer } from "@/app/components/auth-button-server";
+import { ComposePost } from "../components/compose-post";
 
 export default function Login (){
     return (
         <section className="grid place-content-center min-h-screen">
             <h1 className="text-xl font-bold mb-4">Bienvenido a Xclone</h1>
             <AuthButtonServer />
+           
             </section>
     )
 }
