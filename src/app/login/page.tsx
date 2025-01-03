1ef9507091e3944bca248970432e46db79dd8609
Stefano <hemetmas@gmail.com>
2025-01-03 14:36:59 +0100

    .

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,7 +6,6 @@ export default function Login (){
         <section className="grid place-content-center min-h-screen">
             <h1 className="text-xl font-bold mb-4">Bienvenido a Xclone</h1>
             <AuthButtonServer />
-           
             </section>
     )
 }
