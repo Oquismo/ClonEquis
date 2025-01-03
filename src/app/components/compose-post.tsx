5dd2f887f65f6f8296d373946a3f9bbaf0a63d8c
Stefano <hemetmas@gmail.com>
2025-01-03 20:34:36 +0100

    Update compose-post.tsx

--- src/app/components/compose-post.tsx
+++ src/app/components/compose-post.tsx
@@ -1,43 +1,35 @@
-import React, { useState } from 'react';
+// import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
+// import { cookies } from "next/headers";
 
+// import { revalidatePath } from "next/cache";
 export function ComposePost ({
+    
     userAvatarUrl
 }: {
+    
     userAvatarUrl: string;
 }){
-    const [message, setMessage] = useState<string | null>(null);
+    const addPost = async (formData: FormData) => {
+        'use server'
+        console.log('hola capullo')
 
-    const addPost = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const formData = new FormData(event.currentTarget);
-        const postContent = formData.get('post');
-
-        try {
-            'use server';
-            console.log('Post content:', postContent);
-            // Aquí puedes agregar la lógica para enviar el post al servidor
-            setMessage('Post publicado con éxito');
-        } catch (error) {
-            console.error('Error al publicar el post:', error);
-            setMessage('Error al publicar el post');
-        }
-    };
-
+    }
     return(
-        <form onSubmit={addPost} className="flex flex-1 flex-col gap-y-4">
-            <img className="rounded-full w-12 h-12 object-contain" src={userAvatarUrl} />
+        <form action={addPost} className=" flex flex-1 flex-col gap-y-4">
+              <img className="rounded-full w-12 h-12 object-contain" src={userAvatarUrl} />
             <div className="flex flex-1 flex-col gap-y-4">
-                <textarea
-                    name='post'
-                    rows={4}
-                    className="w-full text-2xl bg-black placeholder-gray-500"
-                    placeholder="Que dices primo"
-                ></textarea>
-                <button type="submit" className="bg-sky-300 font-bold rounded-full px-5 py-2 self-end">
-                    Publicar tuit
-                </button>
-                {message && <p>{message}</p>}
+
+            <textarea
+            name='post'
+            rows={4}
+            className="w-full textt-2xl bg-black placeholder-gray-500"
+            placeholder="Que dices primo"
+            ></textarea>
+            <button type="submit" className="bg-sky-300 font-bold rounded-full px-5 py-2 self-end"
+            >Publicar tuit
+            </button>
             </div>
         </form>
-    );
+    )
+
 }
