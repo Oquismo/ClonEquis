775aa387e27512969a127b78f970e77a1cd5c6c1
Stefano <hemetmas@gmail.com>
2025-01-06 18:50:51 +0100

    Update compose-post.tsx

--- src/app/components/compose-post.tsx
+++ src/app/components/compose-post.tsx
@@ -1,38 +1,29 @@
 import { cookies } from "next/headers";
 import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
-import React, { useState } from 'react';
+import React from 'react';
 
 export function ComposePost ({
     userAvatarUrl
 }: {
     userAvatarUrl: string;
 }){
-    const [message, setMessage] = useState<string | null>(null);
-
-    const addPost = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const formData = new FormData(event.currentTarget);
+    const addPost = async (formData: FormData) => {
+        'use server';
         const content = formData.get('content');
 
         if (content === null) return;
 
-        try {
-            const supabase = createServerActionClient({ cookies });
+        const supabase = createServerActionClient({ cookies });
 
-            const { data: { user } } = await supabase.auth.getUser();
+        const { data: { user } } = await supabase.auth.getUser();
 
-            if (user === null) return;
+        if (user === null) return;
 
-            await supabase.from('posts').insert({ content, user_id: user.id });
-            setMessage('Post publicado con éxito');
-        } catch (error) {
-            console.error('Error al publicar el post:', error);
-            setMessage('Error al publicar el post');
-        }
+        await supabase.from('posts').insert({ content, user_id: user.id });
     };
 
     return (
-        <form onSubmit={addPost} className="flex flex-1 flex-col gap-y-4">
+        <form action={addPost} className="flex flex-1 flex-col gap-y-4">
             <img className="rounded-full w-12 h-12 object-contain" src={userAvatarUrl} />
             <div className="flex flex-1 flex-col gap-y-4">
                 <textarea
@@ -44,7 +35,6 @@ export function ComposePost ({
                 <button type="submit" className="bg-sky-300 font-bold rounded-full px-5 py-2 self-end">
                     Publicar
                 </button>
-                {message && <p>{message}</p>}
             </div>
         </form>
     );
