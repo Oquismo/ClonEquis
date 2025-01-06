c17eb52e72fe2de01448b372b0ea09d70b6727c1
Stefano <hemetmas@gmail.com>
2025-01-06 18:36:43 +0100

    Update compose-post.tsx

--- src/app/components/compose-post.tsx
+++ src/app/components/compose-post.tsx
@@ -1,49 +1,41 @@
-
 import { cookies } from "next/headers";
-
 import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
+import React from 'react';
 
-// import { revalidatePath } from "next/cache";
 export function ComposePost ({
-    
     userAvatarUrl
 }: {
-    
     userAvatarUrl: string;
 }){
     const addPost = async (formData: FormData) => {
-        'use server'
-        const content = formData.get('content') 
+        'use server';
+        const content = formData.get('content');
 
-        if (content === null) return
+        if (content === null) return;
 
-        const supabase = createServerActionClient ({cookies})
+        const supabase = createServerActionClient({ cookies });
 
-        const { data: {user} } = await supabase.auth.getUser()
+        const { data: { user } } = await supabase.auth.getUser();
 
-        if (user === null) return
+        if (user === null) return;
 
-        await supabase.from('posts').insert({content, user_id: user.id})
-           
-        
+        await supabase.from('posts').insert({ content, user_id: user.id });
+    };
 
-    }
-    return(
-        <form action={addPost} className=" flex flex-1 flex-col gap-y-4">
-              <img className="rounded-full w-12 h-12 object-contain" src={userAvatarUrl} />
+    return (
+        <form action={addPost} className="flex flex-1 flex-col gap-y-4">
+            <img className="rounded-full w-12 h-12 object-contain" src={userAvatarUrl} />
             <div className="flex flex-1 flex-col gap-y-4">
-
-            <textarea
-            name='content'
-            rows={4}
-            className="w-full textt-2xl bg-black placeholder-gray-500"
-            placeholder="Que dices primo"
-            ></textarea>
-            <button type="submit" className="bg-sky-300 font-bold rounded-full px-5 py-2 self-end"
-            >Publicar tuit
-            </button>
+                <textarea
+                    name='content'
+                    rows={4}
+                    className="w-full text-2xl bg-black placeholder-gray-500"
+                    placeholder="Que dices primo"
+                ></textarea>
+                <button type="submit" className="bg-sky-300 font-bold rounded-full px-5 py-2 self-end">
+                    Publicar tuit
+                </button>
             </div>
         </form>
-    )
-
+    );
 }
