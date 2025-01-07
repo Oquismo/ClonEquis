8b570c4d37e29ed9a7217dc72ad04b30865a4552
Stefano <hemetmas@gmail.com>
2025-01-08 00:12:13 +0100

    Update post-list.tsx

--- src/app/components/post-list.tsx
+++ src/app/components/post-list.tsx
@@ -1,36 +1,47 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+import PostCard from "./post-card"
 
-import React from 'react';
-import PostCard from './post-card'; // Asegúrate de que la ruta sea correcta
+// Define el tipo 'post' si no está definido en otro lugar
+type User = {
+  user_name: string;
+  name: string;
+  avatar_url: string;
+};
 
 type Post = {
   id: string;
+  user: User;
   content: string;
-  user: {
-    userName: string;
-    userFullName: string;
-    avatar_url: string;
-  };
-  created_at: string;
+  created_at: string; // Añadir el campo de fecha de publicación
 };
 
-/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
-export function PostList({ posts }: { posts: Post[] }) {
-  return (
-    <>
-      {posts.map(({ id, content, user, created_at }) => {
-        const { userName, userFullName, avatar_url: avatarUrl } = user;
-        return (
-          <PostCard 
-            content={content}
-            key={id} 
-            userName={userName} 
-            userFullName={userFullName} 
-            avatarUrl={avatarUrl}
-            createdAt={created_at} // Pasar la fecha de publicación a PostCard
-          />
-        );
-      })}
-    </>
-  );
+export function PostList ({ posts }: { posts: Post[] }) {
+    return (
+        <>
+        {
+          posts?.map(post => {
+            const {
+              id,
+              user,
+              content,
+              created_at, // Obtener la fecha de publicación
+            } = post;
+            const {
+              user_name: userName,
+              name: userFullName,
+              avatar_url: avatarUrl,
+            } = user;
+            return (
+              <PostCard 
+                content={content}
+                key={id} 
+                userName={userName} 
+                userFullName={userFullName} 
+                avatarUrl={avatarUrl}
+                createdAt={created_at} // Pasar la fecha de publicación a PostCard
+              />
+            );
+          })
+        }
+        </>
+    );
 }
