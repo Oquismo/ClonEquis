442ee8c0ea2680efc80c1e0d996f370b05969a38
Stefano <hemetmas@gmail.com>
2025-01-08 00:25:12 +0100

    .

--- src/app/components/post-list.tsx
+++ src/app/components/post-list.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import PostCard from "./post-card"
 
 // Define el tipo 'post' si no está definido en otro lugar
@@ -14,34 +15,61 @@ type Post = {
   created_at: string; // Añadir el campo de fecha de publicación
 };
 
-export function PostList ({ posts }: { posts: Post[] }) {
-    return (
-        <>
-        {
-          posts?.map(post => {
-            const {
-              id,
-              user,
-              content,
-              created_at, // Obtener la fecha de publicación
-            } = post;
-            const {
-              user_name: userName,
-              name: userFullName,
-              avatar_url: avatarUrl,
-            } = user;
-            return (
-              <PostCard 
-                content={content}
-                key={id} 
-                userName={userName} 
-                userFullName={userFullName} 
-                avatarUrl={avatarUrl}
-                createdAt={created_at} // Pasar la fecha de publicación a PostCard
-              />
-            );
-          })
-        }
-        </>
-    );
+export function PostList() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    fetch("https://clon-equis.vercel.app/?_rsc=1h9g0", {
+      headers: {
+        accept: "*/*",
+        "accept-language": "es-ES,es;q=0.9,en;q=0.8",
+        "next-router-state-tree": "%5B%22%22%2C%7B%22children%22%3A%5B%22__PAGE__%22%2C%7B%7D%2C%22%2F%22%2C%22refetch%22%5D%7D%5D",
+        priority: "u=1, i",
+        rsc: "1",
+        "sec-ch-ua": "\"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Windows\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin"
+      },
+      referrer: "https://clon-equis.vercel.app/",
+      referrerPolicy: "strict-origin-when-cross-origin",
+      body: null,
+      method: "GET",
+      mode: "cors",
+      credentials: "include"
+    })
+    .then(response => response.json())
+    .then(data => setPosts(data))
+    .catch(error => console.error('Error fetching posts:', error));
+  }, []);
+
+  return (
+    <>
+      {posts?.map(post => {
+        const {
+          id,
+          user,
+          content,
+          created_at, // Obtener la fecha de publicación
+        } = post;
+        const {
+          user_name: userName,
+          name: userFullName,
+          avatar_url: avatarUrl,
+        } = user;
+        return (
+          <PostCard 
+            content={content}
+            key={id} 
+            userName={userName} 
+            userFullName={userFullName} 
+            avatarUrl={avatarUrl}
+            createdAt={created_at} // Pasar la fecha de publicación a PostCard
+          />
+        );
+      })}
+    </>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { redirect } from "next/navigation";
 import { PostList } from "./components/post-list";
 import { ComposePost } from "./components/compose-post";
 
+
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies });
   const {
@@ -13,21 +14,23 @@ export default async function Home() {
 
   if (session === null) {
     redirect("/login");
-    return null; // Asegúrate de que la función se detenga aquí
   }
 
   const { data: posts } = await supabase
-    .from("posts")
-    .select("*, user:users(name, avatar_url, user_name)");
+  .from("posts")
+  .select("*, user:users(name, avatar_url, user_name)");
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <section className="max-w-[600px] border-l border-r border-white/30 h-full min-h-screen">
+    <main className="flex min-h-screen flex-col items-center justify-between ">
+      <section className="max-w-[600px]  border-l border-r border-white/30 h-full min-h-screen">
+        
         <ComposePost userAvatarUrl={session.user?.user_metadata?.avatar_url} />
         <PostList posts={posts ?? []} />
+        <AuthButtonServer />
       </section>
-      <AuthButtonServer />
-      <main className="mt-4 items-center justify-between"></main>
+      
+      <main className="mt-4 items-center justify-between">
+      </main>
     </main>
   );
 }
