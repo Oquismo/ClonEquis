8734b94d6c14dd328ef5d8f108597258690e47de
Stefano <hemetmas@gmail.com>
2025-01-08 00:34:38 +0100

    uo

--- src/app/components/compose-post.tsx
+++ src/app/components/compose-post.tsx
@@ -1,42 +1,31 @@
-import { cookies } from "next/headers";
-import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
-import React from 'react';
+'use client'
+
+import { ComposePostButton } from './compose-post-button'
+import { addPost } from '../../acctions/add-post-action'
+import { useRef } from 'react'
 
 export function ComposePost ({
-    userAvatarUrl
+  userAvatarUrl
 }: {
-    userAvatarUrl: string;
-}){
-    const addPost = async (formData: FormData) => {
-        'use server';
-        const content = formData.get('content');
+  userAvatarUrl: string
+}) {
+  const formRef = useRef<HTMLFormElement>(null)
 
-        if (content === null) return;
-
-        const supabase = createServerActionClient({ cookies });
-
-        const { data: { user } } = await supabase.auth.getUser();
-
-        if (user === null) return;
-
-        await supabase.from('posts').insert({ content, user_id: user.id });
-    };
-
-    return (
-        <form action={addPost} className="flex flex-1 flex-col gap-y-4">
-            
-            <div className="flex flex-1 flex-col gap-y-4">
-            <img className="rounded-full w-12 h-12 object-contain" src={userAvatarUrl} />
-                <textarea
-                    name='content'
-                    rows={4}
-                    className="w-full text-2xl bg-black placeholder-gray-500"
-                    placeholder="Que dices primo"
-                ></textarea>
-                <button type="submit" className="bg-sky-300 font-bold rounded-full px-5 py-2 ">
-                    Publicar
-                </button>
-            </div>
-        </form>
-    );
+  return (
+    <form ref={formRef} action={async (formData) => {
+      await addPost(formData)
+      formRef.current?.reset()
+    }} className='flex flex-row p-3 border-b border-white/20'>
+      <img className='rounded-full w-10 h-10 object-contain mr-4' src={userAvatarUrl} />
+      <div className='flex flex-1 flex-col gap-y-4'>
+      <textarea
+        name='content'
+        rows={4}
+        className='w-full text-xl bg-black placeholder-gray-500 p-2'
+        placeholder='¡¿Qué está pasando!?'
+      ></textarea>
+        <ComposePostButton />
+      </div>
+    </form>
+  )
 }
