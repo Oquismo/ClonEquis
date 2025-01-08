da6492fbf2319e443c10f0a2aea8eb439aba4fcf
Stefano <hemetmas@gmail.com>
2025-01-08 16:42:57 +0100

    Update auth-button-server.tsx

--- src/app/components/auth-button-server.tsx
+++ src/app/components/auth-button-server.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
+// import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+// import { cookies } from "next/headers";
 import { AuthButton } from './auth-button-client';
+import { Session } from '@supabase/auth-js';
+
+// Define el tipo de la sesión utilizando el tipo de @supabase/auth-js
+let sessionCache: Session | null = null;
 
 // Función para obtener datos del servidor
-async function fetchSession() {
+async function fetchSession(): Promise<Session> {
+  if (sessionCache) {
+    return sessionCache;
+  }
+
   const response = await fetch('/api/auth');
-  const session = await response.json();
+  const session: Session = await response.json();
+  sessionCache = session;
   return session;
 }
 
@@ -14,9 +23,6 @@ async function fetchSession() {
 export async function AuthButtonServer() {
   // Obtener datos del servidor
   const session = await fetchSession();
-
-  // Crea una instancia del cliente de Supabase para el servidor
-  const supabase = createServerComponentClient({ cookies });
 
   return (
     <div>
