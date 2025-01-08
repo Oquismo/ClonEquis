c9afac3d7431dcdce12aeaee818f81b8a27590e3
Stefano <hemetmas@gmail.com>
2025-01-08 16:00:24 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { AuthButtonServer } from "@/app/components/auth-button-server";
 import { redirect } from "next/navigation";
-// import { PostList } from "./components/post-list";
+import { PostList } from "./components/post-list";
 // import { ComposePost } from "./components/compose-post";
 
 
@@ -16,16 +16,16 @@ export default async function Home() {
     redirect("/login");
   }
 
-  // const { data: posts } = await supabase
-  // .from("posts")
-  // .select("*, user:users(name, avatar_url, user_name)");
+  const { data: posts } = await supabase
+  .from("posts")
+  .select("*, user:users(name, avatar_url, user_name)");
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between ">
       <section className="max-w-[600px] mx-auto border-l border-r border-white/30 h-full min-h-screen">
         
-        {/* <ComposePost userAvatarUrl={session.user?.user_metadata?.avatar_url} />
-        <PostList posts={posts ?? []} /> */}
+        {/* <ComposePost userAvatarUrl={session.user?.user_metadata?.avatar_url} />*/}
+        <PostList posts={posts ?? []} /> 
         <AuthButtonServer />
       </section>
       
