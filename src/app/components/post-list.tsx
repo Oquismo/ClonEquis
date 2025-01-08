3040afb96c57e4329d9c5ce4eb0a027dd07f8a88
Stefano <hemetmas@gmail.com>
2025-01-08 02:00:23 +0100

    Update post-list.tsx

--- src/app/components/post-list.tsx
+++ src/app/components/post-list.tsx
@@ -1,6 +1,12 @@
-import PostCard from "./post-card"
+'use client'
+import { Card, CardHeader, CardBody, CardFooter, Avatar, Button } from "@nextui-org/react";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { IconMessageCircle } from '@tabler/icons-react';
+import { IconRepeat } from '@tabler/icons-react';
+import { IconHeartPlus } from '@tabler/icons-react';
+import PostCard from "./post-card";
 
-// Define el tipo 'post' si no está definido en otro lugar
 type User = {
   user_name: string;
   name: string;
@@ -13,32 +19,43 @@ type Post = {
   content: string;
 };
 
-export function PostList ({ posts }: { posts: Post[] }) {
-    return (
-        <>
-        {
-          posts?.map(post => {
-            const {
-              id,
-              user,
-              content,
-            } = post;
-            const {
-              user_name: userName,
-              name: userFullName,
-              avatar_url: avatarUrl,
-            } = user;
-            return (
-              <PostCard 
-                content={content}
-                key={id} 
-                userName={userName} 
-                userFullName={userFullName} 
-                avatar_url={avatarUrl}
-              />
-            );
-          })
+export function PostList({ posts }: { posts: Post[] }) {
+  const [requestMade, setRequestMade] = useState(false);
+  const [fetchedPosts, setFetchedPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    if (!requestMade) {
+      const fetchPosts = async () => {
+        try {
+          const response = await fetch("https://clon-equis.vercel.app/api/posts", {
+            method: "GET",
+            headers: {
+              accept: "application/json",
+            },
+          });
+          const data = await response.json();
+          setFetchedPosts(data.posts);
+          setRequestMade(true); // Marca que la solicitud ya se ha realizado
+        } catch (error) {
+          console.error("Error fetching posts:", error);
         }
-        </>
-    );
+      };
+
+      fetchPosts();
+    }
+  }, [requestMade]);
+
+  return (
+    <>
+      {fetchedPosts.map((post) => (
+        <PostCard
+          key={post.id}
+          userName={post.user.user_name}
+          userFullName={post.user.name}
+          avatar_url={post.user.avatar_url}
+          content={post.content}
+        />
+      ))}
+    </>
+  );
 }
