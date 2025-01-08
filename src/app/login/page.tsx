b467e7bbe342551db2e442b0bda0e913516c2250
Stefano <hemetmas@gmail.com>
2025-01-08 01:39:01 +0100

    /././.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,36 +1,11 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
 import { AuthButtonServer } from "@/app/components/auth-button-server";
-import { redirect } from "next/navigation";
-import { PostList } from "../components/post-list";
-import { ComposePost } from "../components/compose-post"
 
 
-export default async function Home() {
-  const supabase = createServerComponentClient({ cookies });
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
-
-  if (session === null) {
-    redirect("/login");
-  }
-
-  const { data: posts } = await supabase
-  .from("posts")
-  .select("*, user:users(name, avatar_url, user_name)");
-
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between ">
-      <section className="max-w-[600px] mx-auto border-l border-r border-white/30 h-full min-h-screen">
-        
-        <ComposePost userAvatarUrl={session.user?.user_metadata?.avatar_url} />
-        <PostList posts={posts ?? []} />
-        <AuthButtonServer />
-      </section>
-      
-      <main className="mt-4 items-center justify-between">
-      </main>
-    </main>
-  );
+export default function Login (){
+    return (
+        <section className="grid place-content-center min-h-screen">
+            <h1 className="text-xl font-bold mb-4">Bienvenido a Xclone</h1>
+            <AuthButtonServer />
+            </section>
+    )
 }
