6e93ec79c57cf74a092a68154d137a13ea353f41
Stefano <hemetmas@gmail.com>
2025-01-09 15:19:18 +0100

    delete

--- src/app/components/post-list.tsx
+++ src/app/components/post-list.tsx
@@ -1,63 +1,41 @@
 'use client'
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import PostCard from './post-card';
 
-// Define el tipo 'User' si no está definido en otro lugar
-type User = {
-  user_name: string;
-  name: string;
-  avatar_url: string;
-};
+interface Post {
+    id: string;
+    content: string;
+    user: {
+        name: string;
+        avatar_url: string;
+        user_name: string;
+    };
+}
 
-// Define el tipo 'Post' si no está definido en otro lugar
-type Post = {
-  id: string;
-  user: User;
-  content: string;
-};
+interface PostListProps {
+    posts: Post[];
+}
 
-export function PostList() {
-  const [fetchedPosts, setFetchedPosts] = useState<Post[]>([]);
-  const [requestMade, setRequestMade] = useState(false);
+export function PostList({ posts }: PostListProps) {
+    const [fetchedPosts, setFetchedPosts] = useState(posts);
 
-  useEffect(() => {
-    if (!requestMade) {
-      const fetchPosts = async () => {
-        try {
-          const response = await fetch('/api/posts', {
-            headers: {
-              accept: 'application/json',
-            },
-          });
-          const data = await response.json();
-          setFetchedPosts(data.posts);
-          setRequestMade(true); // Marca que la solicitud ya se ha realizado
-        } catch (error) {
-          console.error('Error fetching posts:', error);
-        }
-      };
+    const handlePostDelete = (id: string) => {
+        setFetchedPosts(fetchedPosts.filter(post => post.id !== id));
+    };
 
-      fetchPosts();
-    }
-  }, [requestMade]);
-
-  const handlePostDelete = (id: string) => {
-    setFetchedPosts(fetchedPosts.filter(post => post.id !== id));
-  };
-
-  return (
-    <>
-      {fetchedPosts.map(post => (
-        <PostCard
-          key={post.id}
-          id={post.id}
-          userName={post.user.user_name}
-          userFullName={post.user.name}
-          avatar_url={post.user.avatar_url}
-          content={post.content}
-          onDelete={handlePostDelete}
-        />
-      ))}
-    </>
-  );
+    return (
+        <>
+            {fetchedPosts.map(post => (
+                <PostCard
+                    key={post.id}
+                    id={post.id}
+                    userName={post.user.user_name}
+                    userFullName={post.user.name}
+                    avatar_url={post.user.avatar_url}
+                    content={post.content}
+                    onDelete={handlePostDelete}
+                />
+            ))}
+        </>
+    );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
-import { AuthButtonServer } from "@/app/components/auth-button-server";
-import { redirect } from "next/navigation";
-import { PostList } from "./components/post-list";
-import { ComposePost } from "./components/compose-post";
-
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+import {ComposePost} from './components/compose-post';
+import {PostList} from './components/post-list';
+import {AuthButtonServer} from './components/auth-button-server';
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies });
@@ -19,39 +18,20 @@ export default async function Home() {
   const { data: posts, error } = await supabase
     .from('posts')
     .select('*, user:users(name, avatar_url, user_name)')
-    .order('created_at', { ascending: false })
+    .order('created_at', { ascending: false });
 
   if (error) {
-    console.error(error)
-    return <div>Error al cargar los posts</div>
+    console.error(error);
+    return <div>Error al cargar los posts</div>;
   }
 
   return (
-
-
     <main className="flex min-h-screen flex-col items-center justify-between">
-    <section className="max-w-[600px] w-full mx-auto border-l border-r border-white/20 min-h-screen">
-      <ComposePost userAvatarUrl={session.user?.user_metadata?.avatar_url} />
-      <PostList posts={posts ?? []} />
-    </section>
-    <AuthButtonServer />
-  </main>
-
-
-
-
-
-
-    // <main className="flex min-h-screen flex-col items-center justify-between ">
-    //   <section className="max-w-[600px] mx-auto border-l border-r border-white/30 h-full min-h-screen">
-        
-    //     <ComposePost userAvatarUrl={session.user?.user_metadata?.avatar_url} />
-    //     <PostList posts={posts ?? []} />
-    //     <AuthButtonServer />
-    //   </section>
-      
-    //   <main className="mt-4 items-center justify-between">
-    //   </main>
-    // </main>
+      <section className="max-w-[600px] w-full mx-auto border-l border-r border-white/20 min-h-screen">
+        <ComposePost userAvatarUrl={session.user?.user_metadata?.avatar_url} />
+        <PostList posts={posts ?? []} />
+      </section>
+      <AuthButtonServer />
+    </main>
   );
 }
